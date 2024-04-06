114057ae2d93efec06b04f48cc1689d033795136
devsaif-byte <saifuddinrabib654@gmail.com>
2024-04-07 03:38:00 +0600

    feat: add a new getuser function

--- routes/user-router.js
+++ routes/user-router.js
@@ -1,5 +1,10 @@
 import express from "express";
-import { login, logout, register } from "../controllers/user-controller.js";
+import {
+  getUser,
+  login,
+  logout,
+  register,
+} from "../controllers/user-controller.js";
 import { isAuthorized } from "../middlewares/auth-middleware.js";
 
 const router = express.Router();
@@ -7,5 +12,5 @@ const router = express.Router();
 router.post("/register", register);
 router.post("/login", login);
 router.get("/logout", isAuthorized, logout);
-
+router.get("/get-user", isAuthorized, getUser);
 export default router;
