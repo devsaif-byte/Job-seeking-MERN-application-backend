a134ccbe72dee8b2ce8b8752b11f99a4f250e30a
devsaif-byte <saifuddinrabib654@gmail.com>
2024-04-07 03:41:15 +0600

    feat: add routes

--- routes/job-router.js
+++ routes/job-router.js
@@ -1,5 +1,18 @@
 import express from "express";
-
+import { isAuthorized } from "../middlewares/auth-middleware.js";
+import {
+  deleteJob,
+  getAllJobs,
+  getMyJobs,
+  postJob,
+  updateJob,
+} from "../controllers/job-controller.js";
 const router = express.Router();
+
+router.get("/all-jobs", getAllJobs);
+router.post("/post", isAuthorized, postJob);
+router.get("/my-jobs", isAuthorized, getMyJobs);
+router.put("/update/:id", isAuthorized, updateJob);
+router.delete("/delete/:id", isAuthorized, deleteJob);
 
 export default router;
