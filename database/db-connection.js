ce15ebe4b4bd01f787ff00ccc6fd9fcaf9c34382
devsaif-byte <saifuddinrabib654@gmail.com>
2024-04-19 05:54:54 +0600

    fix: asynchonous db connection

--- database/db-connection.js
+++ database/db-connection.js
@@ -1,12 +1,26 @@
 import mongoose from "mongoose";
 
-export const dbConnection = () => {
-  mongoose
-    .connect(process.env.MONGO_URI, {
-      dbName: "MERN_JOB_SEEKER_APP",
-    })
-    .then(() => console.log("Connected to database!"))
-    .catch((err) =>
-      console.log(`Error occured while connecting to database: ${err}`)
-    );
+// const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@job-app-cluster.ego8yki.mongodb.net/?retryWrites=true&w=majority&appName=JOB-APP-CLUSTER`;
+const clientOptions = {
+  dbName: "MERN_JOB_SEEKER_APP",
+  authSource: "admin",
+  user: "saif007",
+  pass: "DeSwCc1CWq1aqj3L",
+  tls: true,
+  tlsAllowInvalidCertificates: true,
+  serverApi: { version: "1", strict: true, deprecationErrors: true },
 };
+
+export const dbConnection = async () => {
+  try {
+    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+    await mongoose.connect(process.env.MONGO_URI, clientOptions);
+    console.log("Successfully connected to MongoDB!");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await mongoose.disconnect();
+  }
+};
+dbConnection();
