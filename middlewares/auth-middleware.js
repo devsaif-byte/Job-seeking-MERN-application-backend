afc39c0dc5efd5bb385df5c4748fad65dd8219c4
devsaif-byte <saifuddinrabib654@gmail.com>
2024-04-20 12:25:44 +0600

    chore: sending allow origin access

--- middlewares/auth-middleware.js
+++ middlewares/auth-middleware.js
@@ -11,10 +11,20 @@ import { User } from "../models/user-model.js";
  * @returns {Promise<void>} - A Promise that resolves once the authorization check is complete.
  */
 export const isAuthorized = catchAsyncError(async (req, res, next) => {
-  const { token } = req.cookies;
-  if (!token) return next(new ErrorHandler("User not authorized", 400));
-  const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
-  req.user = await User.findById(decoded.id);
+  const token = req.cookies.token || "";
+  try {
+    if (!token) return next(new ErrorHandler("User not authorized", 401));
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    req.user = await User.findById(decoded.id);
 
-  next();
+    res.header("Access-Control-Allow-Origin", req.headers.origin);
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+
+    next();
+  } catch (error) {
+    return res.status(500).json(err.toString());
+  }
 });
