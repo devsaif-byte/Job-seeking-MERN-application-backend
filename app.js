736a09ede769fbe40647d263faa67baffc1cef88
devsaif-byte <saifuddinrabib654@gmail.com>
2024-04-23 04:56:18 +0600

    chore: cors config

--- app.js
+++ app.js
@@ -12,20 +12,16 @@ import cookieSession from "cookie-session";
 const app = express();
 dotenv.config({ path: "./configs/config.env" });
 
-app.use(
-  // using cors
-  cors({
-    origin: [
-      process.env.FRONTEND_URL_DEV_MODE,
-      process.env.FRONTEND_URL_PROD_MODE,
-    ],
-    methods: ["GET", "POST", "DELETE", "PUT"],
-    credentials: true,
-  }),
-  // using cookie parser
-  cookieParser()
-);
-
+const corsOptions = {
+  origin: [
+    process.env.FRONTEND_URL_PROD_MODE,
+    process.env.FRONTEND_URL_DEV_MODE,
+  ],
+  methods: ["GET", "POST", "DELETE", "PUT"],
+  credentials: true,
+};
+app.use(cors(corsOptions));
+app.use(cookieParser());
 app.use(
   // using cookie session
   cookieSession({
@@ -43,35 +39,6 @@ app.use(
     ),
   })
 );
-export const allowCors = (fn) => async (req, res) => {
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    process.env.FRONTEND_URL_PROD_MODE
-  );
-  // another common pattern
-  // res.setHeader("Access-Control-Allow-Origin", req.headers.origin);
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-  );
-  if (req.method === "OPTIONS") {
-    res.status(200).end();
-    return;
-  }
-  return await fn(req, res);
-};
-
-const handler = (req, res) => {
-  const d = new Date();
-  res.end(d.toString());
-};
-
-allowCors(handler);
 
 app.use(
   // using json, urlencode, fileupload for cloudinary
@@ -80,9 +47,9 @@ app.use(
   fileUpload({ useTempFiles: true, tempFileDir: "/tmp/" })
 );
 // using routes
-app.use("/api/v1/job", jobRouter);
-app.use("/api/v1/user", userRouter);
-app.use("/api/v1/application", applicationRouter);
+app.use("/api/v1/job", cors(corsOptions), jobRouter);
+app.use("/api/v1/user", cors(corsOptions), userRouter);
+app.use("/api/v1/application", cors(corsOptions), applicationRouter);
 // calling database function
 dbConnection();
 // using custom error middleware
