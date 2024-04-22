8f156ec9a128a49374c9fd2d445472f64e20c8f2
devsaif-byte <saifuddinrabib654@gmail.com>
2024-04-23 04:30:30 +0600

    chore: setup allow origins

--- app.js
+++ app.js
@@ -45,9 +45,12 @@ app.use(
 );
 export const allowCors = (fn) => async (req, res) => {
   res.setHeader("Access-Control-Allow-Credentials", true);
-  // res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    process.env.FRONTEND_URL_PROD_MODE
+  );
   // another common pattern
-  res.setHeader("Access-Control-Allow-Origin", req.headers.origin);
+  // res.setHeader("Access-Control-Allow-Origin", req.headers.origin);
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET,OPTIONS,PATCH,DELETE,POST,PUT"
